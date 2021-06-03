7cedc166004f6970e1fd3bec67f2758d3fe6273d
Dante <danter@uw.edu>
2021-06-03 13:42:05 -0700

    Adds test for my_lm

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+penguins <- NewPackageLecture9::my_penguins
+
+penguins <- penguins %>% na.omit
+
+frm_1 <- penguins %>% dplyr::select(bill_length_mm,
+                                                           bill_depth_mm,
+                                                           flipper_length_mm,
+                                                           body_mass_g)
+test_that("my_lm works mathematically", {
+  expect_type(my_lm(body_mass_g ~ bill_length_mm + bill_depth_mm, frm_1), "double")
+})
+
+test_that("my_lm throws proper errors", {
+  expect_error(my_lm("string" ~ "string2" + "string3", frm_1))
 })
--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
+
+test_that("my_lm works mathematically", {
   expect_equal(2 * 2, 4)
 })
